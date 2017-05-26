37804fabbe65654b09b1836d5512367e5fe49808
Charlie Weems <charlie.weems@gmail.com>
2017-05-26 15:24:11 -0700

    Change static directory

--- sendcertified/settings.py
+++ sendcertified/settings.py
@@ -127,13 +127,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '../main/static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, '../main/static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(PROJECT_ROOT, '../main/static'),
+#)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
