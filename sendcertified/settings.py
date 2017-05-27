4cc97a8cab7aa5065ab7c1c385085d9fa8380ffe
Charlie Weems <charlie.weems@gmail.com>
2017-05-27 11:46:59 -0700

    Change allowed hosts

--- sendcertified/settings.py
+++ sendcertified/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost"]
+ALLOWED_HOSTS = [".sendcertified.co", ".herokuapp.com", "localhost"]
 
 
 # Application definition
